e241e7dd8115225540ece302d5b9bc3ea105a94b
Rigel Iksandy Marthana <55847476+RigelIksandy08@users.noreply.github.com>
2020-04-08 14:40:03 +0700

    Update aboutme.php

--- aboutme.php
+++ aboutme.php
@@ -2,6 +2,7 @@
 echo "Nama         : Rigel Iksandy Marthana <br>";
 echo "Kelas        : XI RPL 2  <br>";
 echo "Asal Sekolah : SMK Taruna Bhakti <br>";
+echo "Alamat Rumah : Jln Jatijajar RT07/01 No 22 <br>";
 
 
 ?>
